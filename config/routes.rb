178c4fd3140b52ec11fb47d452ebab3b73c10794
Talha Javed <talha.javed@square63.org>
2018-10-30 19:53:10 +0500

    member route added for user activation

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   root 'projects#index'
   devise_for :users
   namespace :admin do
-    resources :users
-    patch "/users/set_user_activiation/:id", to: "users#set_user_activiation", as: :user_activation
+    resources :users do
+      member do
+        patch 'set_user_activiation'
+      end
+    end
   end
   resources :clients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html  
