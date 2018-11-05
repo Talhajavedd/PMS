bfb779999e046fb4208871746ddb84009a9ed044
Talha Javed <talha.javed@square63.org>
2018-11-05 15:26:49 +0500

    time logs added for admin

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,9 @@ Rails.application.routes.draw do
       resources :payments do
         get 'delete'
       end
+    resources :time_logs do
+      get 'delete'
+    end
     end
   end
   resources :clients do
