f02f87ee0bef76b4c46109ee4e519f27e55dbccf
Talha Javed <talha.javed@square63.org>
2018-11-05 12:29:47 +0500

    comments model added(comma mistake in user model corrected)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :comments, as: :commentable,
+  has_many :comments, as: :commentable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
