13612c38f3606e19c1cf971e46ff8572268d65db
talha <talha.javed@square63.org>
2018-11-14 17:46:43 +0500

    Payment: pundit authorization added

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,23 +1,22 @@
 class PaymentsController < ApplicationController
-  before_action do
-    set_project
-    authenticate_manager
-  end
+  before_action :set_project
   before_action :set_payment, only: %i[edit update destroy]
 
   def index
     @payments = @project.payments.all
-    @project_name = @project.name
+    authorize Payment
   end
 
   def new
     @payment = @project.payments.new
+    authorize @payment
   end
 
   def edit; end
 
   def create
     @payment = @project.payments.new(payment_params)
+    authorize @payment
     flash.now[:notice] = 'Payment succesfully created!' if @payment.save
   end
 
@@ -47,6 +46,7 @@ class PaymentsController < ApplicationController
 
   def set_payment
     @payment = @project.payments.find(params[:id])
+    authorize @payment
   end
 
   def payment_params
