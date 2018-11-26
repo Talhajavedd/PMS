ce7307b54191abd8c7a543aa0b49be6aeae6d74a
talha <talha.javed@square63.org>
2018-11-26 19:07:48 +0500

    Welcome: eager loading added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    @max_earning_projects = Project.left_outer_joins(:payments).group(:name).order("sum(payments.amount) DESC").first(5)
-    @min_earning_projects = Project.left_outer_joins(:payments).group(:name).order("sum(payments.amount)").first(5)
+    @max_earning_projects = Project.includes(:client).left_outer_joins(:payments).group(:name).order("sum(payments.amount) DESC").first(5)
+    @min_earning_projects = Project.includes(:client).left_outer_joins(:payments).group(:name).order("sum(payments.amount)").first(5)
 
     @payments = Project.joins(:payments).group(:name).where(payments: {created_at: Date.today.beginning_of_month..Date.today.end_of_month} ).sum(:amount)
     @time_logs = Project.joins(:time_logs).group(:name).where(time_logs: {created_at: Date.today.beginning_of_month..Date.today.end_of_month} ).sum(:hours)
