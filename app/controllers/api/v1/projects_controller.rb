6cc64d78faa32e091c42ac3d6827ce1745af7855
talha <talha.javed@square63.org>
2018-11-26 19:06:15 +0500

    Project: serializer added to projects api

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,17 +1,25 @@
 class Api::V1::ProjectsController < Api::APIController
   before_action :set_project, only: %i[show]
+  before_action :set_serializer_options, only: %i[show]
 
   def index
-    successful_response( Project.search_with(params, @current_user) )
+    successful_response(Project.search_with(params, @current_user))
   end
 
   def show
     unsucessful_response(404, 'Not Authorized') unless @project
 
-    successful_response(@project)
+    successful_response(ProjectSerializer.new(@project, @options).serializable_hash)
   end
 
   private
+
+  def set_serializer_options
+    @options = { params: {} }
+    @options[:params][:comment_page] = params[:comment_page]
+    @options[:params][:time_log_page] = params[:time_log_page]
+    @options[:include] = [:client, :users, :payments]
+  end
 
   def set_project
     if @current_user.user?
