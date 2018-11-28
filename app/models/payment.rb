93d1723d558f37fa874450a9650fc819e06e9889
talha <talha.javed@square63.org>
2018-11-28 19:15:40 +0500

    Payment: upper limit added

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,7 +1,7 @@
 class Payment < ApplicationRecord
   belongs_to :project
 
-  validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 999999999 }
 
   paginates_per 10
 end
